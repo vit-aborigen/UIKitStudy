406c39c9bab4a955dffe47d966eb28b9c757ffbf
vit-aborigen <vit.aborigen@gmail.com>
2022-11-06 20:32:51 +0300

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by VITALI KAZLOU on 6.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
