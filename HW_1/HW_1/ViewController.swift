3559856ce7036be9308f75e34f8cb87018c42915
vit-aborigen <vit.aborigen@gmail.com>
2022-12-15 23:41:48 +0300

    Init

---
+++ HW_1/HW_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_1
+//
+//  Created by VITALI KAZLOU on 12/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
