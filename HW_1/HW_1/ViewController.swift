9a912f473d538e6a05793f0052cc4fbdd87e2cdb
vit-aborigen <vit.aborigen@gmail.com>
2022-12-17 23:26:47 +0300

    Greetings block done

--- HW_1/HW_1/ViewController.swift
+++ HW_1/HW_1/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var GreetingsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        GreetingsLabel.text = ""
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        showAlert(title: "Please enter your name", message: "", style: .alert)
     }
 
-
+    func showAlert(title: String, message: String, style: UIAlertController.Style) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        let action = UIAlertAction(title: "OK", style: .default, handler: { action in
+            let userName = alert.textFields?.first?.text ?? ""
+            self.GreetingsLabel.text = "Hi, " + userName
+        })
+        
+        alert.addAction(action)
+        alert.addTextField()
+        present(alert, animated: true)
+    }
 }
-
