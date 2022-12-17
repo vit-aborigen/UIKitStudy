7ef2be2769a4c1c1810a7f6e3578963aa3ad4157
vit-aborigen <vit.aborigen@gmail.com>
2022-12-17 23:58:13 +0300

    simple math

--- HW_1/HW_1/ViewController.swift
+++ HW_1/HW_1/ViewController.swift
@@ -9,21 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var GreetingsLabel: UILabel!
+    @IBOutlet weak var summLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         GreetingsLabel.text = ""
+        summLabel.isHidden = true
     }
     
     override func viewDidAppear(_ animated: Bool) {
         showAlert(title: "Please enter your name", message: "", style: .alert)
     }
 
+    @IBAction func doSomeMath(_ sender: Any) {
+        let mathAlert = UIAlertController(title: "Add numbers", message: "Please specify your values", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default, handler: { action in
+            let value1 = mathAlert.textFields?.first?.text ?? ""
+            let value2 = mathAlert.textFields?.last?.text ?? ""
+            self.summLabel.text = value1 + value2
+            self.summLabel.isHidden = false
+        })
+        
+        mathAlert.addAction(action)
+        mathAlert.addTextField()
+        mathAlert.addTextField()
+        
+        self.present(mathAlert, animated: true)
+    }
+    
+    @IBAction func guessTheNumberStart(_ sender: Any) {
+        print("Guess")
+    }
+    
     func showAlert(title: String, message: String, style: UIAlertController.Style) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: style)
-        let action = UIAlertAction(title: "OK", style: .default, handler: { action in
+        let action = UIAlertAction(title: "OK", style: .default, handler: { _ in
             let userName = alert.textFields?.first?.text ?? ""
             self.GreetingsLabel.text = "Hi, " + userName
+            
         })
         
         alert.addAction(action)
