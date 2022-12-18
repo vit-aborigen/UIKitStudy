7345123cde06424a148e027b1b34dd1975248ade
vit-aborigen <vit.aborigen@gmail.com>
2022-12-18 13:32:43 +0300

    hw4 init

---
+++ HW_4/HW_4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_4
+//
+//  Created by VITALI KAZLOU on 12/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
