f3c060d1c14283746a1d60d7b27a6716ff419ddf
vit-aborigen <vit.aborigen@gmail.com>
2023-01-10 23:41:26 +0300

    init

--- HW_4/HW_4/ViewController.swift
+++ HW_4/HW_4/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let mySwitch = UISwitch()
+    let myButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.mySwitch.frame = CGRect.zero
+        self.mySwitch.center = self.view.center
+        
+        self.myButton.frame = CGRect(x: 100, y: 200, width: 100, height: 200)
+        self.myButton.backgroundColor = .blue
+        self.myButton.setTitle("OK", for: .normal)
+        self.myButton.setTitle("Pressed", for: .highlighted)
+        
+        self.view.addSubview(mySwitch)
+        self.view.addSubview(myButton)
+        
+        self.mySwitch.tintColor = .green
+        self.mySwitch.thumbTintColor = .red
+        self.mySwitch.onTintColor = .black
+        
+        self.myButton.addTarget(self, action: #selector(pressButton(target: )), for: .allTouchEvents)
     }
-
+    
+    @objc func pressButton(target: UIButton) {
+        self.mySwitch.isOn = true
+    }
 
 }
 
