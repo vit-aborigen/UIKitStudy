43f596d5ad285bc2b8e3083979fe5de5d5861f39
vit-aborigen <vit.aborigen@gmail.com>
2023-01-13 23:28:28 +0300

    Init

---
+++ IAmPoor/IAmPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmPoor
+//
+//  Created by VITALI KAZLOU on 1/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
